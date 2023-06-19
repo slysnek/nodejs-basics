d596459fc79c653ac44d2287ac78a3c62fad9d16
Kirill <bloodwingest@gmail.com>
2023-06-20 01:07:46 +0300

    feat: worker done

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,39 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+import { cpus } from "node:os";
+import { resolve } from "node:path";
+import { Worker, isMainThread, parentPort, workerData } from "node:worker_threads"
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const cpuCount = cpus().length
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const workerPath = path.join(__dirname, './worker.js');
+
+const performCalculations = async () => {
+    const workerPromises = [];
+
+    for (let i = 10; i < cpuCount + 10; i++) {
+        workerPromises.push(createWorker(i))
+    }
+
+    function createWorker(cpuCore){
+        return new Promise((resolve, reject) => {
+            const worker = new Worker(workerPath, {
+                workerData: cpuCore,
+            });
+
+            worker.on('message', (data) => resolve({status: 'resolved', data: data}));
+            worker.on('error', () => reject({status: 'error', data: null}));
+            worker.on('exit', (code) => {
+                if (code !== 0)
+                    reject(new Error(`Worker stopped with exit code ${code}`));
+            });
+        })
+    }
+
+    const results = await Promise.allSettled(workerPromises)
+    results.forEach((val) => {
+        console.log(val.value || val.reason);
+    })
+}
 await performCalculations();
