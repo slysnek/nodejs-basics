f5268184ad67639fd353220d35e9f185b335257a
Kirill <bloodwingest@gmail.com>
2023-06-19 20:43:31 +0300

    feat: read.js done

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import { createReadStream } from 'fs';
+import { join } from 'path';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, 'files/fileToRead.txt')
+    const readStream = createReadStream(filePath, 'utf-8')
+
+    readStream.on('error', () => { throw new Error('FS operation failed') })
+    readStream.on('data', data => console.log(data))
 };
 
 await read();
