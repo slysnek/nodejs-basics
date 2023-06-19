da3d4f9873f6ef1085125a5426eb33c19bb9bf4e
Kirill <bloodwingest@gmail.com>
2023-06-19 19:50:44 +0300

    feat: rename.js done

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,26 @@
-const rename = async () => {
-    // Write your code here 
+import { rename, access } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const folderPath = path.join(__dirname, 'files')
+const fileName = 'wrongFilename.txt'
+const newFileName = 'properFilename.md'
+const filePath = path.join(folderPath, fileName)
+const newFilePath = path.join(folderPath, newFileName)
+
+const renameFile = async () => {
+    access(newFilePath, err => {
+        if (!err) {
+            throw new Error('FS operation failed')
+        } else {
+            rename(filePath, newFilePath, (err) => {
+                if (err) throw new Error('FS operation failed')
+                console.log("File has been renamed");
+            });
+        }
+    })
 };
 
-await rename();
+await renameFile();
